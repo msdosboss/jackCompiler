bcfb0799345eb99d17585fd7a51b0857479b8edc
Jacob Todd <jacobt0722@gmail.com>
2026-04-27 10:15:25 -0700

    fixed bug in neg that was the same as not's bug

--- codeWriter.py
+++ codeWriter.py
@@ -43,7 +43,7 @@ ARITHMETIC_TRANSLATIONS = {
     "neg" : [
                 "@SP",
                 "AM=M-1",
-                "-M",
+                "M=-M",
                 "@SP",
                 "M=M+1"
             ],
